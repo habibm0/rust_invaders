85c3943fec9410f98681c9549d5b2e96ff72722f
Mohammed Habibullahi Mohammed <habibullahimohammem@gmail.com>
2022-11-19 17:14:40 +0100

    change bevy version to 0.7

--- src/main.rs
+++ src/main.rs
@@ -3,5 +3,14 @@
 use bevy::prelude::*; 
 
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .insert_resource(ClearColor(Color::rgb(0.04, 0.04, 0.04)))
+        .insert_resource(WindowDescriptor {
+            width: 598.0,
+            height: 676.0,
+            title: "Rust Invaders!".to_string(),
+            ..Default::default()
+        })
+        .add_plugins(DefaultPlugins)
+        .run();
 }
